60c29fab7ff46cc630c242a8ea095dccec734790
Katsura Fujita <katsura.code@outlook.com>
2025-12-09 17:40:24 +0900

    fix: remove vite-plugin-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
-import vercel from "vite-plugin-vercel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson(), vercel()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
   server: {
     proxy: {
       "/api": {
