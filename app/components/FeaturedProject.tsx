dcb8f44af1223c22f9653f404a2419b373270f8a
Katsura Fujita <katsura.code@outlook.com>
2025-12-10 16:39:13 +0900

    feat: add FeaturedProject component

---
+++ app/components/FeaturedProject.tsx
@@ -0,0 +1,9 @@
+const FeaturedProject = () => {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold text-gray-200 mb-6">🏰 Featured Projects</h2>
+    </section>
+  );
+};
+
+export default FeaturedProject;
