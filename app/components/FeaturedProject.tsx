00b525d82ce908e5d4efc39f6b8115e092654ea1
Katsura Fujita <katsura.code@outlook.com>
2025-12-10 17:01:44 +0900

    feat: add FeatureProject component to display featured ProjectCard on home page

--- app/components/FeaturedProject.tsx
+++ app/components/FeaturedProject.tsx
@@ -1,7 +1,22 @@
-const FeaturedProject = () => {
+import type { Project } from "~/type";
+import ProjectCard from "./ProjectCard";
+
+type FeaturedProjectProps = {
+  projects: Project[];
+  count: number;
+};
+
+const FeaturedProject = ({ projects, count }: FeaturedProjectProps) => {
+  const featured = projects.filter((project) => project.featured).slice(0, count);
   return (
     <section>
       <h2 className="text-3xl font-bold text-gray-200 mb-6">🏰 Featured Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {featured.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </section>
   );
 };
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,3 +1,5 @@
+import FeaturedProject from "~/components/FeaturedProject";
+import type { Project } from "~/type";
 import type { Route } from "./+types";
 
 export const meta = ({}: Route.MetaArgs) => {
@@ -7,8 +9,30 @@ export const meta = ({}: Route.MetaArgs) => {
   ];
 };
 
-const Home = () => {
-  return <section>HomePage</section>;
+export const loader = async ({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> => {
+  try {
+    const apiUrl =
+      process.env.NODE_ENV === "development"
+        ? `${import.meta.env.VITE_API_URL}/projects`
+        : "https://friendly-4l2uojve2-katsuracodes-projects.vercel.app/projects";
+
+    const res = await fetch(apiUrl);
+    if (!res.ok) throw new Error("Failed to fetch data from the server.");
+
+    const data = await res.json();
+    console.log(data);
+
+    return { projects: data };
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    return { projects: [] };
+  }
 };
 
-export default Home;
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+
+  return <FeaturedProject projects={projects} count={2} />;
+};
+
+export default HomePage;
